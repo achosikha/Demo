caa59e674a370c1b07317f2407c207cbfe788bb5
achosikha <softdevelopment@yahoo.com>
2024-08-01 23:00:15 +0400

    My first project on GitHub

--- src/MyMain.java
+++ src/MyMain.java
@@ -1,2 +1,7 @@
 public class MyMain {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Git & GitHub...");
+
+        System.out.println("Additional information on Git & GitHub...");
+    }
 }
