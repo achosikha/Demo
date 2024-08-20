2d829c4369cf4c1b04f703e1c6a1fd0b449930b4
achosikha <softdevelopment@yahoo.com>
2024-08-20 09:04:56 +0400

    Deleted

--- src/MyMain.java
+++ src/MyMain.java
@@ -1,9 +1,5 @@
 public class MyMain {
     public static void main(String[] args) {
-        System.out.println("Welcome to Git & GitHub...");
-
-        System.out.println("Additional information on Git & GitHub...");
-
-        System.out.println("Just to show you how it works....");
+        // Lets do a GITHUB
     }
 }
--- src/masterpackage/NewDocument.java
+++
@@ -1,7 +0,0 @@
-package masterpackage;
-
-public class NewDocument {
-    public static void main(String[] args) {
-        // Comments
-    }
-}
